c1e4ec2df6a95db077f6e880cafe623f959e1aaf
Viorodm <55165290+Viorodm@users.noreply.github.com>
2019-09-10 22:34:29 -0600

    Documentos de funciones
    Estos documentos corresponden a una función de dos valores que suma, resta y multiplica, además de su respectivo documento de testeo.

---
+++ tarea2.py
@@ -0,0 +1,13 @@
+# A=int(input("Introduzca el valor de A: "))
+# B=int(input("Introduzca el valor de B: "))
+
+
+def Calculos(A, B):
+    Resta = A-B
+    Suma = A+B
+    Multiplicacion = A*B
+    return Resta, Suma, Multiplicacion
+
+# resta,suma,multiplicacion = Calculos(A,B)
+# print("La resta corresponde a ", resta, ", la suma a ", suma,
+# ", y la multiplicacion a ",multiplicacion)
